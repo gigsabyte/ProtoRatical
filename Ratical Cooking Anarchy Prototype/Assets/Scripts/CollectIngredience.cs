bc0a60b31f4bef9d282858c47d2767657855bce4
Gigi Bachtel <gbachtel@ucsc.edu>
2020-06-25 17:24:54 -0700

    Collect Ingredience scripts

--- Ratical Cooking Anarchy Prototype/Assets/Scripts/CollectIngredience.cs
+++ Ratical Cooking Anarchy Prototype/Assets/Scripts/CollectIngredience.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectIngredience : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class CollectIngredience : MonoBehaviour
     float initXscale;
     BoxCollider2D boxCollider;
     public float ingredienceCount = 0;
+
+    public Text ingredientText;
+
+    public StateManager sm;
+    public GameObject done;
 
 
     // Start is called before the first frame update
@@ -21,6 +27,7 @@ public class CollectIngredience : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (ingredienceCount >= 3) return;
         if(Input.GetKey(KeyCode.D))
         {
             transform.localScale = new Vector3(initXscale,  transform.localScale.y, transform.localScale.z);
@@ -33,6 +40,7 @@ public class CollectIngredience : MonoBehaviour
             float dist = runspeed * Time.deltaTime;
             ingredience.transform.Translate(dist, 0, 0);
         }
+
         if(Input.GetKeyDown(KeyCode.Space))
         {
             Collider2D[] overlap = Physics2D.OverlapAreaAll(boxCollider.bounds.min, boxCollider.bounds.max);
@@ -43,6 +51,12 @@ public class CollectIngredience : MonoBehaviour
                     Debug.Log("found valid ingredient");
                     ingredienceCount++;
                     Destroy(col.gameObject);
+                    ingredientText.text = ingredienceCount + "/3 ingredients";
+                    if(ingredienceCount == 3)
+                    {
+                        sm.SetDone();
+                        done.SetActive(true);
+                    }
                 }
             }
         }
