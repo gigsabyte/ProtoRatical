60c7afe36c50e2553115166b7a02e58da7dffa4c
Gigi Bachtel <gbachtel@ucsc.edu>
2020-06-25 17:25:57 -0700

    Lighting cauldron scripts + assets

---
+++ Ratical Cooking Anarchy Prototype/Assets/Scripts/CountFire.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CountFire : MonoBehaviour
+{
+
+    BoxCollider2D boxCollider;
+
+    public float fireCount = 0;
+    public Text countext;
+
+    public StateManager sm;
+    public GameObject done;
+
+    bool donelighting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (donelighting) return;
+        Collider2D[] overlap = Physics2D.OverlapAreaAll(boxCollider.bounds.min, boxCollider.bounds.max);
+        fireCount = overlap.Length - 1;
+        countext.text = fireCount + "/6 flames";
+        if(fireCount >= 6)
+        {
+            donelighting = true;
+            done.SetActive(true);
+            sm.SetDone();
+        }
+    }
+}
---
+++ Ratical Cooking Anarchy Prototype/Assets/Scripts/DragFire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragFire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousepos.z = 0;
+        transform.position = mousepos;
+    }
+}
