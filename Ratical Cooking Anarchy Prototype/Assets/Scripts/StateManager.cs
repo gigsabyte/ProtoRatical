431ead8e31037939aac21eab741abf596e6cba45
Gigi Bachtel <gbachtel@ucsc.edu>
2020-06-25 17:26:17 -0700

    State manager for transitioning through minigames

---
+++ Ratical Cooking Anarchy Prototype/Assets/Scripts/StateManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    int gamesDone = 0;
+    int state = 0;
+
+    public GameObject collect;
+    public GameObject light;
+    public GameObject stir;
+    public GameObject chop;
+
+    public GameObject start;
+    public GameObject done;
+
+    bool cancontinue = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collect.SetActive(false);
+        light.SetActive(false);
+        stir.SetActive(false);
+        chop.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        collect.SetActive(true);
+        light.SetActive(true);
+        start.SetActive(false);
+    }
+
+    public void SetDone()
+    {
+        gamesDone++;
+        if(state == 0 && gamesDone == 2)
+        {
+            collect.SetActive(false);
+            light.SetActive(false);
+            gamesDone = 0;
+            state = 1;
+            StartCoroutine(Wait());
+        }
+        else if(state == 1 && gamesDone >= 1)
+        {
+            stir.SetActive(false);
+            chop.SetActive(false);
+            done.SetActive(true);
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1.0f);
+        stir.SetActive(true);
+        chop.SetActive(true);
+    }
+}
